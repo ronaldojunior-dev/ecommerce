341a5f8ce69024fb628e35b61edebe8514e0d104
NexJuninho <ronaldojunior.dvp@outlook.com>
2026-09-08 19:51:00 -0300

    craida lista de produtos

---
+++ src/data/produtos.js
@@ -0,0 +1,38 @@
+export const produtosIniciais = [
+  {
+    id: '1',
+    nome: 'Fone de Ouvido Bluetooth',
+    precoUnitario: 149.90,
+    quantidade: 1,
+  },
+  {
+    id: '2',
+    nome: 'Teclado Mecânico RGB',
+    precoUnitario: 299.00,
+    quantidade: 1,
+  },
+  {
+    id: '3',
+    nome: 'Mousepad Extra Grande',
+    precoUnitario: 79.90,
+    quantidade: 2,
+  },
+  {
+    id: '4',
+    nome: 'Mouse Sem Fio',
+    precoUnitario: 289.90,
+    quantidade: 1,
+  },
+  {
+    id: '5',
+    nome: 'Monitor 24 Polegadas',
+    precoUnitario: 899.90,
+    quantidade: 1,
+  },
+  {
+    id: '6',
+    nome: 'Stream Deck Mini',
+    precoUnitario: 159.90,
+    quantidade: 2,
+  },
+];
